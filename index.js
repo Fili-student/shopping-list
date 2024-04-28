ed2fab106acbe4779571d3e6633d31fc726cfbaa
Fili-student <triantafillia.filippou@dci-student.org>
2024-04-28 19:26:06 +0200

    fast fertig

--- index.js
+++ index.js
@@ -1,28 +1,49 @@
-// Einkaufsliste: 
-// das muss :
-// -was hinzufügen
-//- was löschen
+//- responsive seite
 
-// bonus:
-//(-alles löschen?)
-// verschiedene seiten mit farbschema?
-
-// Farbschema: cute , dark, healthy or fitness? für serien mit staffel und ep
-// mehrere machen ?
-// responsive seite
-
-const input = document.querySelector("input");
+const input = document.querySelector("#myInput");
 const button = document.querySelector("button");
+const ul = document.querySelector(".todo-list")
+const todoForm = document.getElementById('todo-form');
+const alles = document.querySelector("#alles")
+// const weg = "            X";
+todoForm.addEventListener('submit', function(event) {
+    event.preventDefault(); // Verhindert das neuladen
+});
 
-button.addEventListener("click", (element) => {
-          element.target.style.backgroundColor = "rgb(204, 193, 44)";
-    
+   function wasDazu(){
+    let itemText = input.value.trim(); //remove white spaces
+        if(itemText !== ""){// wenn text nicht leer ist
+            let newItem = document.createElement("li");//neues li kreieren
+            newItem.textContent = itemText; //content des li
+            ul.appendChild(newItem);//das li ins ul packen
+             input.value = "";//damit das input feld wieder leer ist
+            }
+          }
+input.addEventListener("keydown", (event) => {
+    if (event.keyCode === 13) {//13 steht für ENTER
+        wasDazu();// aufrufen wenn enter gedrückt
+    }
+
+
+  ul.addEventListener('click', function(event) {
+    if (event.target.tagName === "LI") { // Check if the clicked element is a list item
+    ul.removeChild(event.target);
+    }
+
+
+
+
   });
-  
+  });
 
-  function wasDazu(){
-    input.add = "";
-  }
-  function wasWeg(){
-    input.remove = "";
-  }
+
+
+  alles.addEventListener("click", function() {
+    wegMitAllem();
+    });
+
+function wegMitAllem() {
+    ul.innerHTML = ""; 
+}
+
+
